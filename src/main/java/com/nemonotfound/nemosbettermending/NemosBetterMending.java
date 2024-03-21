3ad783190603369926271c04c0e35b6e26079b80
NemoNotFound <unconfigured@null.spigotmc.org>
2024-03-21 18:17:28 +0100

    Add compatibility for Clumps

--- src/main/java/com/nemonotfound/nemosbettermending/NemosBetterMending.java
+++ src/main/java/com/nemonotfound/nemosbettermending/NemosBetterMending.java
@@ -1,21 +1,19 @@
 package com.nemonotfound.nemosbettermending;
 
+import com.nemonotfound.nemosbettermending.helper.ClumpsRepairGearsHelper;
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NemosBetterMending implements ModInitializer {
 
 	public static final String MOD_ID = "nemos-better-mending";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger log = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		log.info("Thank you for using Nemo's Better Mending!");
 
-		LOGGER.info("Thank you for using Nemo's Better Mending!");
+		ClumpsRepairGearsHelper.registerRepairEvent();
 	}
 }
